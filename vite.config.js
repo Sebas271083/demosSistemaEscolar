1815368c4d6174beade2ee9e8dfc7a66ef092a41
sebas271083 <sebdelgado83@gmail.com>
2026-05-21 08:29:05 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
