d3c9ff6ccb6cb52faf2c8b93fec2beab42b631c2
sebas271083 <sebdelgado83@gmail.com>
2026-05-21 11:25:39 -0300

    add allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  preview: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: ['demo.sistemaescolar.com.ar', '147.182.128.247']
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    target: 'esmodnext'
   }
 })
